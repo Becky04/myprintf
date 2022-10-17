27b9cc29e0fd1895f035398e88fc6d1ad5f1bb30
Becky <temmyisijola@gmail.com>
2022-10-17 14:41:33 -0700

    added header file

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdarg.h>
@@ -15,4 +15,25 @@ int print_int(va_list ap);
 int print_unsigned_int(va_list ap);
 int print_octal_number(va_list ap);
 int print_hexadecimal(va_list ap);
-int print_hexadecimal_cap(va
+int print_hexadecimal_cap(va_list ap);
+int print_hexadecimal_all(va_list ap, char c);
+
+/**
+ * struct conv_spec - struct op
+ * @spec: specifier
+ * @func: function associated with specifier
+ */
+struct conv_spec
+{
+	char spec;
+	int (*func)(va_list);
+};
+
+/**
+ * typedef struct conv_spec conv_spec_t - struct op
+ * @conv_spec: the struct
+ * @conv_spec_t: name associated to the struct
+ */
+typedef struct conv_spec conv_spec_t;
+
+#endif /* MAIN_H */
