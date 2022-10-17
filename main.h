eab8c04c17199bdf46fbd8aaea019c83cccc8ac7
Godswill Kalu <messagelordwill@gmail.com>
2022-10-17 22:58:43 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 
 int _putchar(char c);
@@ -19,9 +19,10 @@ int print_hexadecimal_cap(va_list ap);
 int print_hexadecimal_all(va_list ap, char c);
 
 /**
- * struct conv_spec - struct op
- * @spec: specifier
- * @func: function associated with specifier
+ * struct conv_spec - Struct op
+ *
+ * @spec: The specifier.
+ * @func: The function associated with the specifier.
  */
 struct conv_spec
 {
@@ -31,8 +32,9 @@ struct conv_spec
 
 /**
  * typedef struct conv_spec conv_spec_t - struct op
- * @conv_spec: the struct
- * @conv_spec_t: name associated to the struct
+ *
+ * @conv_spec: The struct.
+ * @conv_spec_t: The name associated to the struct.
  */
 typedef struct conv_spec conv_spec_t;
 
